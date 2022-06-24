b9b1990fa963ff242e00c15c1bd33f0362646c3b
Gj <gongjie0422@163.com>
2022-06-25 02:45:37 +0800

    fix: modify dashboard

--- src/dashboard/App.ce.vue
+++ src/dashboard/App.ce.vue
@@ -1,21 +1,50 @@
 <template>
   <div class="container operate">
-    <div>{{ x }}</div>
-    <div>{{ y }}</div>
-    <div>{{ sourceType }}</div>
-    <div>{{ env }}</div>
+    <NDescriptions
+      v-for="(env, index) in envs"
+      :key="index"
+      :column="1"
+      size="small"
+      label-placement="left"
+    >
+      <NDescriptionsItem label="_key">
+        {{ env.globalKey }}
+      </NDescriptionsItem>
+      <NDescriptionsItem
+        v-for="(target, key) in env.envs"
+        :key="key"
+        :label="key"
+      >
+        {{ target }}
+      </NDescriptionsItem>
+    </NDescriptions>
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
+<script>
+import { ref, defineComponent } from "vue";
 import { useMouse } from "@vueuse/core";
 import { useCurrentEnv } from "./hooks/chrome";
+import { NDescriptions, NDescriptionsItem } from "naive-ui";
 
-const env = ref({});
-useCurrentEnv().then((target) => (env.value = target));
-
-const { x, y, sourceType } = useMouse();
+export default defineComponent({
+  components: { NDescriptions, NDescriptionsItem },
+  setup() {
+    const envs = ref([]);
+    useCurrentEnv().then((target) => {
+      Object.keys(target).forEach((key) => {
+        envs.value.push({
+          globalKey: key,
+          envs: target[key],
+        });
+      });
+    });
+    const { x, y } = useMouse();
+    return {
+      envs,
+    };
+  },
+});
 </script>
 
 <style>
@@ -23,10 +52,9 @@ const { x, y, sourceType } = useMouse();
   -moz-user-select: none;
   -webkit-user-select: none;
   user-select: none;
-  background-color: rebeccapurple;
-  height: 200px;
-  width: 200px;
+  background-color: rgba(38, 38, 38, 0.5);
   border-radius: 5px;
+  box-shadow: 0 0 1px #f5f5f5;
   padding: 5px;
   display: flex;
   flex-direction: column;
@@ -34,6 +62,7 @@ const { x, y, sourceType } = useMouse();
   top: 0;
   left: 0;
   z-index: 999;
+  color: #fff;
 }
 
 .operate {
--- src/dashboard/hooks/chrome.js
+++ src/dashboard/hooks/chrome.js
@@ -1,14 +1,16 @@
 
-// content devtools 使用缓存KEY
-const __ENV_HOST_KEY__ = window.location.host
-// 环境变量KEY
-const __ENV_CONTENT_KEY__ = `__ENV_CONTENT_KEY__${__ENV_HOST_KEY__}`
-
 // 获取当前启用环境变量
 export function useCurrentEnv() {
     return new Promise((resolve) => {
-        chrome.storage?.local.get([__ENV_CONTENT_KEY__], (res) => {
-            resolve(res[__ENV_CONTENT_KEY__] || {});
-        })
+        window.addEventListener(
+            "message",
+            (event) => {
+                const data = event.data;
+                if (data.type === "__init_envs" && data.to === "document") {
+                    resolve(data.value);
+                }
+            },
+            false
+        );
     })
 }
